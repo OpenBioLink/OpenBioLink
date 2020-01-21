30d76839bd606967ca12ac5b97a3138ac672ea4d
anna <anna@breit-mail.at>
2020-01-21 22:18:38 +0100

    adding additional pykeen models

--- src/openBioLink/evaluation/models/modelTypes.py
+++ src/openBioLink/evaluation/models/modelTypes.py
@@ -2,9 +2,15 @@ from enum import Enum
 import evaluation.models.pykeen_models as pykeen_models
 class ModelTypes(Enum):
 
-    #PYKEEN_TRANS_E = 'TransE_PyKeen'
-    #PYKEEN_TRANS_R = 'TransR_PyKeen'
+    #Model_Name = ModelClass
     TransE_Pykeen = pykeen_models.TransE_PyKeen
     TransR_Pykeen = pykeen_models.TransR_PyKeen
+    TransD_Pykeen = pykeen_models.TransD_PyKeen
+    TransH_Pykeen = pykeen_models.TransH_PyKeen
+    UM_Pykeen = pykeen_models.Unstructured_PyKeen
+    DistMult_Pykeen = pykeen_models.DistMult_PyKeen
+    SE_Pykeen = pykeen_models.SE_PyKeen
+    Rescal_Pykeen = pykeen_models.Rescal_PyKeen
+
 
 
