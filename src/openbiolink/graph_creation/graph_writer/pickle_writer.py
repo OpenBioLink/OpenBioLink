e0c8f0d7cfe0844e9c33e717405e80ab60c570d9
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-02-17 16:38:53 +0100

    Fix typo

--- src/openbiolink/graph_creation/graph_writer/pickle_writer.py
+++ src/openbiolink/graph_creation/graph_writer/pickle_writer.py
@@ -16,10 +16,10 @@ __all__ = [
 
 
 class GraphPickleWriter(GraphWriter):
-    def write(self, *, nodes_dic, tp_edges: Mapping[str, Edge], tp_namespaces, tn_nodes, tn_edges, tn_namespaces):
+    def write(self, *, tp_nodes, tp_edges: Mapping[str, Edge], tp_namespaces, tn_nodes, tn_edges, tn_namespaces):
         """Write the graph as pickles."""
         with open(os.path.join(self.graph_dir_path, "tp_nodes.pkl"), "wb") as file:
-            pickle.dump(nodes_dic, file, protocol=pickle.HIGHEST_PROTOCOL)
+            pickle.dump(tp_nodes, file, protocol=pickle.HIGHEST_PROTOCOL)
         with open(os.path.join(self.graph_dir_path, "tp_edges.pkl"), "wb") as file:
             pickle.dump(tp_edges, file, protocol=pickle.HIGHEST_PROTOCOL)
         with open(os.path.join(self.graph_dir_path, "tp_namespaces.pkl"), "wb") as file:
