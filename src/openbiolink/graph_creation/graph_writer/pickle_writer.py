9a4257069665c3ff6f2cb99a3e9280292fbee02e
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-02-17 16:32:50 +0100

    Add pickle writer
    This writer just saves all the intermediate stuff as pickles. This will make it easier to check intermediate results as well as to quickly iterate on new export methods (rather than having to go through the whole pipeline over and over)

---
+++ src/openbiolink/graph_creation/graph_writer/pickle_writer.py
@@ -0,0 +1,32 @@
+"""A utility for outputting graphs as pickle files.
+
+To test, run ``openbiolink generate --no-download --no-input --output-format pickle --qual hq``.
+"""
+
+import os
+import pickle
+from typing import Mapping
+
+from openbiolink.edge import Edge
+from openbiolink.graph_creation.graph_writer.base import GraphWriter
+
+__all__ = [
+    'GraphPickleWriter',
+]
+
+
+class GraphPickleWriter(GraphWriter):
+    def write(self, *, nodes_dic, tp_edges: Mapping[str, Edge], tp_namespaces, tn_nodes, tn_edges, tn_namespaces):
+        """Write the graph as pickles."""
+        with open(os.path.join(self.graph_dir_path, "tp_nodes.pkl"), "wb") as file:
+            pickle.dump(nodes_dic, file, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(os.path.join(self.graph_dir_path, "tp_edges.pkl"), "wb") as file:
+            pickle.dump(tp_edges, file, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(os.path.join(self.graph_dir_path, "tp_namespaces.pkl"), "wb") as file:
+            pickle.dump(tp_namespaces, file, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(os.path.join(self.graph_dir_path, "tn_nodes.pkl"), "wb") as file:
+            pickle.dump(tn_nodes, file, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(os.path.join(self.graph_dir_path, "tn_edges.pkl"), "wb") as file:
+            pickle.dump(tn_edges, file, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(os.path.join(self.graph_dir_path, "tn_namespaces.pkl"), "wb") as file:
+            pickle.dump(tn_namespaces, file, protocol=pickle.HIGHEST_PROTOCOL)
